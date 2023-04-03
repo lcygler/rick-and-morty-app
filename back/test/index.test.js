ffafddd77d59e8adbb6f4e3492ef65824528ca7d
lcygler <leandrocygler@gmail.com>
2023-04-03 00:37:01 -0300

    update test with async/await

--- back/test/index.test.js
+++ back/test/index.test.js
@@ -4,24 +4,27 @@ const agent = session(app);
 
 describe("Routes testing", () => {
   describe("Route: GET rickandmorty/onsearch/:id", () => {
-    it("Expect status code 200", () => {
-      agent.get("/rickandmorty/onsearch/1").expect(200);
+    it("Expect status code 200", async () => {
+      const response = await agent.get("/rickandmorty/onsearch/1");
+      expect(response.statusCode).toBe(200);
     });
 
-    it("Expect object with properties: 'id', 'name', 'species', 'gender' and 'image'", () => {
-      return agent.get("/rickandmorty/onsearch/1").then((response) => {
-        expect(Object.keys(response.body)).toContain(
-          "id",
-          "name",
-          "species",
-          "gender",
-          "image"
-        );
-      });
+    it("Expect object with properties: 'id', 'name', 'species', 'gender' and 'image'", async () => {
+      const response = await agent.get("/rickandmorty/onsearch/1");
+      expect(Object.keys(response.body)).toContain(
+        "id",
+        "name",
+        "species",
+        "gender",
+        "image"
+      );
     });
 
-    it("Expect status code 500 for errors", () => {
-      agent.get("/rickandmorty/onsearch/IDThatDoesNotExist").expect(500);
+    it("Expect status code 500 for errors", async () => {
+      const response = await agent.get(
+        "/rickandmorty/onsearch/IDThatDoesNotExist"
+      );
+      expect(response.statusCode).toBe(500);
     });
   });
 });
